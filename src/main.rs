c95e735aa1f23df57fb201d67bf20409f96d37bf
leo blume <leonardo.blume@gymnasium-essen-werden.de>
2023-06-09 23:15:43 +0200

    Only 1 mate per output

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
-use shakmaty::{Chess, Position, Outcome};
+use shakmaty::{Chess, Position, Outcome, MoveList};
 use shakmaty::fen::Fen;
+
+fn more_than_one_checkmate(chess: Chess, moves: &MoveList) -> bool {
+    let mut game = chess.clone();
+    moves.iter().map(|move_| {
+        game.play_unchecked(move_);
+        let game_over = game.is_game_over();
+        game = chess.clone();
+        game_over
+    }).filter(|&b| b).nth(1).is_some()
+}
 
 fn random_game() -> Option<Fen> {
     let mut game = Chess::new();
     let mut prev = game.clone();
+    let mut legal_moves = MoveList::new();
 
     while game.outcome().is_none() {
         prev = game.clone();
-        let legal_moves = game.legal_moves();
+        legal_moves = game.legal_moves();
         let random_idx = fastrand::usize(..legal_moves.len());
         game.play_unchecked(
             &legal_moves[random_idx]
@@ -17,6 +28,10 @@ fn random_game() -> Option<Fen> {
     let Some(Outcome::Decisive { winner }) = game.outcome() else {
         return None;
     };
+
+    if more_than_one_checkmate(prev.clone(), &legal_moves) {
+        return None;
+    }
 
     println!("{winner} won");
     println!();
@@ -28,5 +43,7 @@ fn random_game() -> Option<Fen> {
 }
 
 fn main() {
-    random_game();
+    for _ in 0..20 {
+        random_game();
+    }
 }
