ad147edae337e62b94433d3e8f6c12266fdf890e
leo blume <leonardo.blume@gymnasium-essen-werden.de>
2023-06-09 22:53:08 +0200

    Add random_game method and implement loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use shakmaty::{Chess, Position};
+use shakmaty::fen::Fen;
+
+fn random_game() -> Option<Fen> {
+    let mut game = Chess::new();
+
+    while game.outcome().is_none() {
+        let legal_moves = game.legal_moves();
+        let random_idx = fastrand::usize(..legal_moves.len());
+        game.play_unchecked(
+            &legal_moves[random_idx]
+        );
+    }
+
+    todo!()
+}
+
 fn main() {
     println!("Hello, world!");
 }
