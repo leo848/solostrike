0367d9e0932573f003c23d345ae0a7eca47153f0
leo blume <leonardo.blume@gymnasium-essen-werden.de>
2023-06-11 22:18:17 +0200

    Add custom assert function for browser support

---
+++ frontend/src/game/loadFens.ts
@@ -0,0 +1,23 @@
+let fens: null | string[] = null;
+
+export async function randomFen() {
+  if (fens == null) {
+    fens = await loadFens();
+  }
+  assert(fens.length == 100_000, "Length is not 100_000");
+  return fens[Math.floor(Math.random() * fens.length)];
+}
+
+async function loadFens(): Promise<string[]> {
+  let resp = await fetch("fens/fens_l.json");
+  let json = await resp.json();
+  return json;
+}
+
+function assert(condition: boolean, message?: string): (typeof condition extends true ? never : undefined) {
+  if (condition) {
+    throw new Error(message);
+  } else {
+    return undefined;
+  }
+}
